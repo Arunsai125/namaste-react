975d3e5804232821e2d3cb8c537b634de980dfe8
Arun Sai Narla <arunsainarla@Mac.lan>
2025-09-23 21:34:40 -0400

    Initial setup

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const element = React.createElement('h1', {id:"heading"}, 'Hello, world!');
-        ReactDOM.render(element, document.getElementById('root'));
-
-const tree = React.createElement('div', {id:"parent"}, 
-        React.createElement('div', {className:"heading"}), 
-        [React.createElement('h1', {id:"child2"}, "Hello, I'm h1 tag"), React.createElement('h2', {id:"child3"}, "Hello, I'm h2 tag")]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(tree);
+root.render(element);
