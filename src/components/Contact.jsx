89353ac945e492d1801e678e1beeefdb11b4840c
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-22 20:51:19 -0500

    feat: implement Contact component with form and tests for input fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,26 @@
-const Contact = () =>{
-    return(
-        <div className="contact m-10 p-10 text-center">
-            <h1 className="msg"> Hey Fool, This is a dummy app, How dare you to reach out to us ?</h1>
-        </div>
-    );
-}
+const Contact = () => {
+  return (
+    <div className="contact m-10 p-10 text-center">
+      <h1 className="msg">
+        Hey Fool, This is a dummy app, How dare you to reach out to us ?
+      </h1>
+      <form>
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="name"
+        ></input>
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="message"
+        ></input>
+        <button className="border border-black p-2 m-2 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
