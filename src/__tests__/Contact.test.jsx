6e85db2fecfe8fe28fce29d81a7b49146ef199c2
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-23 18:17:55 -0500

    test: refactor Contact component tests to use describe and it blocks for better structure

--- src/__tests__/Contact.test.jsx
+++ src/__tests__/Contact.test.jsx
@@ -2,32 +2,34 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("loads the header", () => {
-  // Rendering
-  render(<Contact />);
+describe("Contact Us Page - Testing", () => {
+  it("loads the header", () => {
+    // Rendering
+    render(<Contact />);
 
-  // Querying
-  const header = screen.getByRole("heading");
+    // Querying
+    const header = screen.getByRole("heading");
 
-  // Assertion
-  expect(header).toBeInTheDocument();
-});
+    // Assertion
+    expect(header).toBeInTheDocument();
+  });
 
-test("loads the input text name", () => {
-  render(<Contact />);
-  const inputName = screen.getByPlaceholderText("name");
-  expect(inputName).toBeInTheDocument();
-});
+  it("loads the input text name", () => {
+    render(<Contact />);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
+  });
 
-test("loads the button", () => {
-  render(<Contact />);
-  const button = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-});
+  it("loads the button", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
 
-test("page containing 2 input text boxes", () => {
-  render(<Contact />);
-  const inputBoxes = screen.getAllByRole("textbox");
-  expect(inputBoxes.length).toBe(2);
-  expect(inputBoxes.length).not.toBe(3);
+  it("page containing 2 input text boxes", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+    expect(inputBoxes.length).not.toBe(3);
+  });
 });
